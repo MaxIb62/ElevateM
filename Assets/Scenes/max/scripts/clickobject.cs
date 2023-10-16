011c733125feb87e40cfa9e19483f9cd8b5798ad
MaxIb62 <cdmx2254@amerike.edu.mx>
2023-10-16 02:25:56 -0600

    shader Outline

---
+++ Assets/Scenes/max/scripts/clickobject.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class clickobject : MonoBehaviour
+{
+    float distance = 10;
+
+    private void OnMouseDrag()
+    {
+        Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance);
+        Vector3 objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+        transform.position = objPosition;
+
+    }
+}
---
+++ Assets/Scenes/max/scripts/pickup.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pickup : MonoBehaviour
+{
+    public GameObject collider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            collider.SetActive(false);
+        }
+    }
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            collider.SetActive(true);
+        }
+    }
+}
