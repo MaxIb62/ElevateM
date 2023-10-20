3fd646735f8f4e0d096a094b9756e5cda1d3a503
MAX <cdmx2254@amerike.edu.mx>
2023-10-19 20:44:24 -0600

    cambios

---
+++ Assets/Scripts/Parent.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parent : MonoBehaviour
+{
+    private Transform parentTransform;
+    private bool isInsideCollider = false;
+
+    private void Start()
+    {
+        parentTransform = GameObject.Find("elevator_box").transform;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("elevatorbox")) // Reemplaza "ColliderTag" con la etiqueta adecuada del Collider.
+        {
+            isInsideCollider = true;
+            transform.SetParent(parentTransform);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (isInsideCollider)
+        {
+            isInsideCollider = false;
+            transform.SetParent(null); // Quitamos al objeto de ser hijo del otro objeto.
+        }
+    }
+}
